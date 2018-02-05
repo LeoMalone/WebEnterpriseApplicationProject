2a4294ad8a3a9203fd5f294af4f44dbc4032f875
LiamM <leomalone.18@gmail.com>
2018-02-05 13:16:15 -0500

    Project creation + Bootstrap

---
+++ LastEver/WebContent/index.jsp
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<title>LastEver</title>
+	
+	<!-- Bootstrap core CSS -->
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+	<!-- Custom styles for this template -->
+    <link href="css/cover.css" rel="stylesheet">
+     <!-- Bootstrap core JavaScript -->
+	<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+	<div id="main">
+		<div id="tray">
+		</div>
+	</div>	
+</body>
+</html>
---
+++ LastEver/src/servlets/LoginServlet.java
@@ -0,0 +1,43 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.LoginDao;
+
+public class LoginServlet extends HttpServlet{
+
+    private static final long serialVersionUID = 1L;
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)  
+            throws ServletException, IOException {  
+
+        response.setContentType("text/html");  
+        PrintWriter out = response.getWriter();        
+        String n=request.getParameter("username");  
+        String p=request.getParameter("password"); 
+        
+        HttpSession session = request.getSession(false);
+        if(session!=null)
+        session.setAttribute("name", n);
+
+        if(LoginDao.validate(n, p)){  
+            RequestDispatcher rd=request.getRequestDispatcher("welcome.jsp");  
+            rd.forward(request,response);  
+        }  
+        else{  
+            out.print("<p style=\"color:red\">Sorry username or password error</p>");  
+            RequestDispatcher rd=request.getRequestDispatcher("index.jsp");  
+            rd.include(request,response);  
+        }  
+
+        out.close();  
+    }  
+}
