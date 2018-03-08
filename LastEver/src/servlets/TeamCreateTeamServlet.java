fa1a0fcc3dd70f0aabe97f5bb05f5dcfa8d4998e
read0086 <read0086@algonquinlive.com>
2018-03-08 00:22:28 -0500

    Team Updates

---
+++ LastEver/src/servlets/TeamCreateTeamServlet.java
@@ -0,0 +1,155 @@
+package servlets;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import beans.DivisionBean;
+import beans.TeamBean;
+import beans.UserBean;
+import dao.TeamCreateTeam;
+import dao.TeamDao;
+import dao.Division;
+
+public class TeamCreateTeamServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 7270142539946516086L;
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
+		response.setContentType("text/html");
+
+
+		String language = null;
+		String userName = null;
+		
+		List<DivisionBean> dlb = new ArrayList<DivisionBean>();
+		Division.getAllDivisions(dlb);
+		request.setAttribute("allDiv", dlb);
+
+		List<TeamBean> tb = new ArrayList<TeamBean>();
+		TeamDao.getAllTeams(tb);
+		request.setAttribute("allTeam", tb);
+
+		if (request.getSession().getAttribute("signedIn") == null) {
+			response.sendRedirect("./login");
+		} else {
+
+			Cookie[] cookies = request.getCookies();
+			if (cookies != null) {
+				for (Cookie cookie : cookies) {
+					if (cookie.getName().equals("username"))
+						userName = cookie.getValue();
+					else if (cookie.getName().equals("language"))
+						language = cookie.getValue();
+				}
+			}
+			if(language == null) {
+				Cookie cookieLanguage = new Cookie("language", "en");
+				cookieLanguage.setMaxAge(60 * 60 * 60 * 30);
+				response.addCookie(cookieLanguage);
+			}
+
+			else {
+
+				language = request.getParameter("language");
+				Cookie[] theCookies = request.getCookies();
+
+				for (Cookie tempCookie : theCookies) {
+					if ("language".equals(tempCookie.getName())) {
+						if (language != null)
+							tempCookie.setValue(language);
+						response.addCookie(tempCookie);
+						break;
+					}
+				}
+
+				request.setAttribute("userName", userName);
+				if (TeamDao.hasTeam(userName)) {
+					response.sendRedirect("teamowner");
+				}
+				else {
+				RequestDispatcher rd = request.getRequestDispatcher("team_create_team.jsp");  
+				rd.forward(request, response);
+				}
+			}
+		}
+	}
+
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		// set response type and get post data from jsp form
+		response.setContentType("text/html");
+
+		String userName = request.getParameter("userName");
+		String newTeamName	 = request.getParameter("newTeamName");
+		String newTeamAbbr = request.getParameter("newTeamAbbr");
+		String newDiv = request.getParameter("divRadio");
+		String language = null;
+		String teamName = request.getParameter("selectTeam");
+		
+		if (TeamDao.hasTeam(userName)) {
+			response.sendRedirect("login");
+		}
+		
+		if (request.getSession().getAttribute("signedIn") == null) {
+			response.sendRedirect("teamCreateTeam");
+		} else {
+
+			Cookie[] cookies = request.getCookies();
+			if (cookies != null) {
+				for (Cookie cookie : cookies) {
+					if (cookie.getName().equals("username"))
+						userName = cookie.getValue();
+					else if (cookie.getName().equals("language"))
+						language = cookie.getValue();
+				}
+			}
+			if(language == null) {
+				Cookie cookieLanguage = new Cookie("language", "en");
+				cookieLanguage.setMaxAge(60 * 60 * 60 * 30);
+				response.addCookie(cookieLanguage);
+			}
+			else {
+
+				language = request.getParameter("language");
+				Cookie[] theCookies = request.getCookies();
+
+				for (Cookie tempCookie : theCookies) {
+					if ("language".equals(tempCookie.getName())) {
+						if (language != null)
+							tempCookie.setValue(language);
+						response.addCookie(tempCookie);
+						break;
+					}
+				}
+
+				// If any parameter is null
+				if(newTeamName == null || newTeamAbbr == null || newDiv == null || newTeamName == "" || newTeamAbbr == "" || newDiv == "") {
+					response.sendRedirect("teamCreateTeam");
+
+				} else {
+					TeamBean team = new TeamBean();
+					team.setTeamName(newTeamName);
+					team.setTeamAbbreviation(newTeamAbbr);
+					team.setDivisionId(newDiv);
+
+					// If createNewUser method returns true
+					if (TeamCreateTeam.createNewTeam(team, userName)) {
+						response.sendRedirect("teamowner");
+
+					} else {
+						response.sendRedirect("teamCreateTeam");
+					}
+				}
+			}
+		}
+	}
+}
---
+++ LastEver/src/servlets/TeamOwnerSelectServlet.java
@@ -0,0 +1,133 @@
+package servlets;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import beans.DivisionBean;
+import beans.TeamBean;
+import dao.Division;
+import dao.TeamDao;
+
+public class TeamOwnerSelectServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
+		response.setContentType("text/html");
+		
+		String teamNameFromParam = null;
+		String userName = null;
+		String language = null;
+		
+		teamNameFromParam = (String) request.getAttribute("selectTeam");
+		if (teamNameFromParam == null) {
+			teamNameFromParam = "coolTeam";
+		}
+		
+		List<DivisionBean> dlb = new ArrayList<DivisionBean>();
+		Division.getAllDivisions(dlb);
+		request.setAttribute("allDiv", dlb);
+		
+		if (request.getSession().getAttribute("signedIn") == null) {
+			response.sendRedirect("./login");
+		} else {
+			Cookie[] cookies = request.getCookies();
+			if (cookies != null) {
+				for (Cookie cookie : cookies) {
+					if (cookie.getName().equals("username"))
+						userName = cookie.getValue();
+					else if (cookie.getName().equals("language"))
+						language = cookie.getValue();
+				}
+			}
+			if(language == null) {
+				Cookie cookieLanguage = new Cookie("language", "en");
+				cookieLanguage.setMaxAge(60 * 60 * 60 * 30);
+				response.addCookie(cookieLanguage);
+			}
+			else {
+	
+				language = request.getParameter("language");
+				Cookie[] theCookies = request.getCookies();
+	
+				for (Cookie tempCookie : theCookies) {
+					if ("language".equals(tempCookie.getName())) {
+						if (language != null)
+							tempCookie.setValue(language);
+						response.addCookie(tempCookie);
+						break;
+					}
+				}		
+
+				
+				request.setAttribute("teamName", teamNameFromParam);
+				request.setAttribute("userName", userName);
+				RequestDispatcher rd = request.getRequestDispatcher("teamowner");  
+		        rd.forward(request, response);	
+			}
+		}
+	}
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response)  throws ServletException, IOException{
+		response.setContentType("text/html");
+		
+		String userName = null;
+		String language = null;
+		
+		String teamName = request.getParameter("selectTeam");
+		
+		if (request.getSession().getAttribute("signedIn") == null) {
+			response.sendRedirect("teamCreateTeam");
+		} else {
+			Cookie[] cookies = request.getCookies();
+			if (cookies != null) {
+				for (Cookie cookie : cookies) {
+					if (cookie.getName().equals("username"))
+						userName = cookie.getValue();
+					else if (cookie.getName().equals("language"))
+						language = cookie.getValue();
+				}
+			}
+			if(language == null) {
+				Cookie cookieLanguage = new Cookie("language", "en");
+				cookieLanguage.setMaxAge(60 * 60 * 60 * 30);
+				response.addCookie(cookieLanguage);
+			}
+			else {
+	
+				language = request.getParameter("language");
+				Cookie[] theCookies = request.getCookies();
+	
+				for (Cookie tempCookie : theCookies) {
+					if ("language".equals(tempCookie.getName())) {
+						if (language != null)
+							tempCookie.setValue(language);
+						response.addCookie(tempCookie);
+						break;
+					}
+				}		
+
+				String url = null;
+				if (teamName == null || teamName == "" || userName == null || userName == "") {
+					url = "teamCreateTeam";
+				}
+				else {
+					url = "teamowner";
+				}
+				request.setAttribute("teamName", teamName);
+				request.setAttribute("userName", userName);
+				RequestDispatcher rd = request.getRequestDispatcher(url);  
+		        rd.forward(request, response);	
+			}
+		}
+	}
+}
