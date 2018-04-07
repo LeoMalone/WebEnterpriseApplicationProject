61407eb9f8974d3eacdcdb7d5ed901f6cc896bb6
Kev <thekpv8@gmail.com>
2018-04-06 21:32:27 -0400

    Added Game Page
    - Added game page
      -> Image used for testing purposes
      -> Image scales based on screen-size
    - Fixed some c:out issues
    - Updated database

---
+++ LastEver/WebContent/js/gamePage.js
@@ -0,0 +1,43 @@
+var cookie = "language";
+var language;
+var cookies = decodeURIComponent(document.cookie);
+var cook = cookies.split(';');
+
+var lang = {en: "//cdn.datatables.net/plug-ins/1.10.16/i18n/English.json", 
+	fr: '//cdn.datatables.net/plug-ins/1.10.16/i18n/French.json'};
+
+for(var i = 0; i < cook.length; i++){
+	var c = cook[i];
+	while(c.charAt(0) == ' '){
+		c = c.substring(1);
+	}
+	if(c.indexOf(cookie) == 0){
+		language = c.substring(cookie.length, c.length);
+		if(language.charAt(0) == '=')
+			language = language.substring(1);
+	}
+}
+
+$(document).ready(function() {
+	$('#hstatistics').DataTable({ 
+		searching: false,
+		order: [[1, "desc"]],
+		paging: false,
+		info: false,
+		"language": {
+			"url": lang[language]
+		}
+		});
+});
+
+$(document).ready(function() {
+	$('#astatistics').DataTable({ 
+		searching: false,
+		order: [[1, "desc"]],
+		paging: false,
+		info: false,
+		"language": {
+			"url": lang[language]
+		}
+		});
+});
